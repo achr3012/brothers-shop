260f76eefb423d914e99a2ada30f940a36d5ad2b
AcHRaF AiSsaOui <achr3012@gmail.com>
2023-12-07 18:49:45 +0100

    Refactor: separating authOptions in his own file

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,29 @@
+import { type AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "AcHRaF && AkRam" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "1",
+          username: "admin",
+          password: process.env.AUTH_PASSWORD as string
+        }
+
+        // If no error and we have user data, return it
+        if (user.username === credentials?.username && user.password === credentials.password) {
+          return user
+        }
+        return null
+      }
+    })
+  ]
+} satisfies AuthOptions
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,6 @@
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
+import { authOptions } from "./authOptions"
 
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "AcHRaF && AkRam" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-
-        const user = {
-          id: "1",
-          username: "admin",
-          password: process.env.AUTH_PASSWORD as string
-        }
-
-        // If no error and we have user data, return it
-        if (user.username === credentials?.username && user.password === credentials.password) {
-          return user
-        }
-
-        return null
-
-      }
-    })
-  ],
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
