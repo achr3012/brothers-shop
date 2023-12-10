dd8344f428bc307914366db65122b1c9c54fcc13
AcHRaF AiSsaOui <achr3012@gmail.com>
2023-12-10 16:03:19 +0100

    Fix El Djeich

---
+++ src/app/El-Djeich/page.tsx
@@ -0,0 +1,23 @@
+import classes from './page.module.css'
+export default function page() {
+  return (
+    <div className={classes.djeich}>
+      <h1>Revue El Djeich 2021</h1>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichJan2021Ar.pdf" target="_blank">01</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichFev2021Ar.pdf" target="_blank">02</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichMar2021Ar.pdf" target="_blank">03</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichAvril2021Ar.pdf" target="_blank">04</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichMai2021Ar.pdf" target="_blank">05</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichJuin2021Ar.pdf" target="_blank">06</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichJuillet2021Ar.pdf" target="_blank">07</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichAout2021Ar.pdf" target="_blank">08</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichSept2021Ar.pdf" target="_blank">09</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichOct2021Ar.pdf" target="_blank">10</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichNov2021Ar.pdf" target="_blank">11</a>
+      <a href="https://www.mdn.dz/site_principal/sommaire/revues/images/EldjeichDec2021Ar.pdf" target="_blank">12</a>
+
+
+      <p>By <a href="https://www.instagram.com/its__achraf" className={classes.achr}>Achr3012</a></p>
+    </div>
+  )
+}
