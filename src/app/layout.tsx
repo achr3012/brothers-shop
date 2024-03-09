69fb2673edb4dc4bc59b105f2663ab51b19ec47b
AcHRaF AiSsaOui <achr3012@gmail.com>
2024-03-09 14:12:22 +0100

    Added metadata and opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import './globals.css'
 
-// import type { Metadata } from 'next'
-// export const metadata: Metadata = {
-//   title: 'Brothers Shop',
-//   description: 'Brothers Shop',
-//   authors: { name: "Achraf Aissaoui" },
-//   openGraph: {
-//     url: "https://abrothers-shop.vercel.app/",
-//     type: "article",
-//     title: 'Brothers Shop',
-//     description: 'Brothers Shop'
-//   }
-// }
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'Brothers Shop',
+  description: 'Brothers Shop',
+  authors: { name: "Achraf Aissaoui" },
+  openGraph: {
+    url: "https://abrothers-shop.vercel.app/",
+    type: "article",
+    title: 'Brothers Shop',
+    description: 'Brothers Shop',
+  }
+}
 
 import { Noto_Sans } from 'next/font/google'
 const noto_sans = Noto_Sans({
